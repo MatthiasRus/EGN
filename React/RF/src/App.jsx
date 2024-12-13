0753eb3cb64d8a8d4b30557dfb3b883f3142735b
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-13 12:26:01 +0400

    Simple greeting Form

--- React/RF/src/App.jsx
+++ React/RF/src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {SayHello as Greeting} from "./greeting.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+const [FormattedData,setFormattedData] = useState(
+    {
+      Firstname:"",
+      Lastname:"",
+      email:"",
+      age:"",
+      sex:"",
+    }
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  <>
 
-export default App
+      <Greeting setFormattedData ={ setFormattedData }/>
+      {FormattedData.Firstname && (
+  <h1>Hello {FormattedData.Firstname} {FormattedData.Lastname}. As per your specification You are a {FormattedData.sex} student age of {FormattedData.age}. Thank you for filling the form we will use {FormattedData.email}
+to contact you in the future regarding any update.
+</h1> 
+
+  )}
+   </>
+  )};
+     
+ export default App
