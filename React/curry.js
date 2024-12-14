f1a316566c0e64650a57960ce4f34e42ad2e5524
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-15 01:10:31 +0400

    This is to upload everything i learned from engineering

---
+++ React/curry.js
@@ -0,0 +1,57 @@
+function curry(f){
+    return function (a){
+        return function (b){
+           return f(a,b)
+        }
+    }
+}
+
+function sum(a,b){
+    return a+b;
+}
+
+let curriedSum = curry(sum)
+
+console.log(curriedSum(1)(2))
+
+
+function letsCurry(curriedFunction){
+    return function(firstParam){
+        return function(secondParam){
+            return function(thirdParam){
+                return curriedFunction(firstParam,secondParam,thirdParam);
+            }
+        }
+    }
+}
+
+function withThreeParam(a,b,c){
+    return a * b + c;
+}
+
+function showingReusabilityOfCurry(a,b,c){
+    return a +" "  + b + " " + c;
+}
+let createCurriedFunction = letsCurry(withThreeParam);
+let reusableCurry = letsCurry(showingReusabilityOfCurry);
+
+console.log(createCurriedFunction(2)(6)(3));
+console.log(reusableCurry("Matthias")("Worku")("Beshah"))
+
+// curry advanced 
+
+function curryTwo(func){
+
+    return function curried(...args){
+        if (args.length >= func.length){
+            return func.apply(this,args);
+        }else{
+            return function (...args2){
+                return curried.apply(this,args.concat(args2))
+            }
+        }
+    }
+}
+
+let newCurry = curryTwo(withThreeParam);
+console.log(newCurry(1,2))
