0f89432f7e3484bdf82ba1510e080b23bf9c59e3
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-16 12:18:41 +0400

    states in react

--- React/ChefClaude/src/cpt/Main.jsx
+++ React/ChefClaude/src/cpt/Main.jsx
@@ -1,13 +1,30 @@
+import {useState} from "react"
+
 export default function Main(){
+
+        const [ingredients, setIngredients] = useState([])
+     const renderIngredients = ingredients?.map(ingredient => <li key={ingredient}>{ingredient}</li>)
+    
+
+    function handleSubmit(e){
+        e.preventDefault();
+        let formData = new FormData(e.currentTarget);
+        let newIngredient = formData.get("ingredient");
+       setIngredients(prevIngredients => [...prevIngredients,newIngredient]);
+           }
+
+
     return (
         <main className="mainContainer">
-            <form className="add-ingredient">
+            <form className="add-ingredient" onSubmit={handleSubmit}>
                 
-                    <input aria-label ="add ingredient" type="text" placeholder="e.g oregano" id="input"/>
+                    <input aria-label ="add ingredient" type="text" placeholder="e.g oregano" id="input" name="ingredient" />
                 
                 <button className="search"> Add ingredient</button>
             </form>
-            
+            <ul className="list-item">
+                {renderIngredients}
+            </ul>
         </main>
     )
 }
--- React/RF/src/App.jsx
+++ React/RF/src/App.jsx
@@ -1,28 +1,9 @@
-import { useState } from 'react'
-import {SayHello as Greeting} from "./greeting.jsx"
-
+import Header from "./components/Header.jsx"
+import './App.css'
 function App() {
-const [FormattedData,setFormattedData] = useState(
-    {
-      Firstname:"",
-      Lastname:"",
-      email:"",
-      age:"",
-      sex:"",
-    }
-  );
-
   return (
-  <>
-
-      <Greeting setFormattedData ={ setFormattedData }/>
-      {FormattedData.Firstname && (
-  <h1>Hello {FormattedData.Firstname} {FormattedData.Lastname}. As per your specification You are a {FormattedData.sex} student age of {FormattedData.age}. Thank you for filling the form we will use {FormattedData.email}
-to contact you in the future regarding any update.
-</h1> 
-
-  )}
-   </>
-  )};
+    <Header/>
+  )
+};
      
  export default App
