865f31758f86bd8e4d60221086a91250df3c19b7
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-05 22:46:24 +0400

    routing demo

--- React/RF/src/main.jsx
+++ React/RF/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import About from './components/About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
         <Routes>
-
+            <Route path='/' element={<App/>} />
+            <Route path='about' element={<About/>}/>
         </Routes>
     </BrowserRouter>
   </StrictMode>
