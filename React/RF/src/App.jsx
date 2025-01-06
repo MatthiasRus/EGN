1d6ef43a60e96d80fb313dd48c105fc86aac444f
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-06 11:04:02 +0400

    designing

--- React/RF/src/App.jsx
+++ React/RF/src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
 function App() {
   return (
-      <h1>Hello World</h1>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 };
      
--- React/RF/src/main.jsx
+++ React/RF/src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import About from './components/About.jsx'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App/>} />
-            <Route path='about' element={<About/>}/>
-        </Routes>
-    </BrowserRouter>
+        <App/>
   </StrictMode>
 )
