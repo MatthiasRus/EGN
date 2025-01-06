f892cb9656350141f50f000e91e90724865a26ff
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-06 11:24:39 +0400

    vans page

--- React/RF/src/App.jsx
+++ React/RF/src/App.jsx
@@ -2,6 +2,9 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
+import './Server.js'
+import Vans from './components/Vans.jsx'
+
 function App() {
   return (
     <BrowserRouter>
@@ -9,10 +12,12 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/vans' element={<Vans/>}/>
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
---
+++ React/RF/src/components/Vans.jsx
@@ -0,0 +1,5 @@
+export default function Vans() {
+  return (
+    <div>Vans</div>
+  )
+}
