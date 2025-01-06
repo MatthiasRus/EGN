ef92511815d70c049b2e5d1227ca633cfb70ed75
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-06 14:23:47 +0400

    using useParam

--- React/RF/src/components/Vans.jsx
+++ React/RF/src/components/Vans.jsx
@@ -14,18 +14,20 @@ export default function Vans() {
     }, [])
 
     const vansElement = vansData.map(van => (
-        <Link key={van.id} to={`/vans/${van.id}`}
+        
+        <div key={van.id} className="van-tile">
+            <Link key={van.id} to={`/vans/${van.id}`}
         aria-label={`View details for ${van.name}, 
                              priced at $${van.price} per day`}>
-        <div key={van.id} className="van-tile">
         <img src={van.imageUrl} alt={`Image of ${van.name}`}  />
         <div className="van-info">
             <h3>{van.name}</h3>
             <p>${van.price}<span>/day</span></p>
         </div>
         <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
     </div>
-    </Link>
+    
 ))
   return (
 
--- React/RF/src/components/vanDefault.jsx
+++ React/RF/src/components/vanDefault.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 export default function VanDefault() {
+    const params = useParams();
+    const [van, setVan] = useState()
+
+    useEffect(() => {
+       ( async function(){
+                const response = await fetch(`/api/vans/${params.id}`);
+                const data = await response.json();
+                console.log(data)
+                setVan(data.vans)
+        })();
+    },[params.id])
   return (
-    <h1>vanDefault</h1>
+    <div className="van-detail-container">
+    {van ? (
+        <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <h2>{van.name}</h2>
+            <p className="van-price"><span>${van.price}</span>/day</p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+        </div>
+    ) : <h2>Loading...</h2>}
+</div>
   )
 }
