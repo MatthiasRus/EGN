64f7da087f42f55adf64150192b2ab2e4e3c15fa
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-06 11:55:33 +0400

    fetching from mirage mock api

--- React/RF/src/components/Vans.jsx
+++ React/RF/src/components/Vans.jsx
@@ -1,5 +1,27 @@
+import React from "react"
+
 export default function Vans() {
+    const [vansData, setVansData] = React.useState([])
+
+    React.useEffect(() => {
+        async function fetchData(){
+          const response = await fetch("api/vans");
+          const data = await response.json();
+          setVansData(data.vans);
+        }
+        fetchData();
+    }, [])
+
   return (
-    <div>Vans</div>
+
+    <div>{
+            vansData.map(van => (
+                <>
+                    <h1 key={van.id}>{van.name} <span><strong><i> { van.price}</i></strong> </span> </h1>
+                    <img src={van.imageUrl} style={{width:"200px", height:"auto"}}></img>
+                    <p key={van.id}>{van.description}</p>
+                </>
+        ))
+        }</div>
   )
 }
