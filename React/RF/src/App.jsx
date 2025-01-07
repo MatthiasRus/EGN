22d54a4cfac3136bf712ce0910df693a34a1ab62
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-07 12:50:12 +0400

    Footer

--- React/RF/src/App.jsx
+++ React/RF/src/App.jsx
@@ -15,14 +15,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes >
-        <Route element={<Layout/>}>
-              <Route path="/" element={<Home />} />
-              <Route path='/vans' element={<Vans/>}/>
-              <Route path="/about" element={<About />} />
-              <Route path="/vans/:id" element={<VanDetail/>}/>
+        <Route path='/' element={<Layout/>}>
+              <Route index element={<div className='Home'><Home /></div>} />
+              <Route path='vans' element={<Vans/>}/>
+              <Route path="about" element={<About />} />
+              <Route path="vans/:id" element={<VanDetail/>}/>
               <Route path='/host' element={<HostLayout/>}>
-                <Route path='/host/income' element={<Income/>}/>
-                <Route path='/host/reviews' element={<Reviews/>}/>
+                    <Route index element={<Dashboard/>}/>
+                    <Route path='income' element={<Income/>}/>
+                    <Route path='reviews' element={<Reviews/>}/>
               </Route>
         </Route>
       </Routes>
