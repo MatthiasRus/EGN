f624087051eeaa404b75ffeb188c636cbf780e71
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2025-01-07 23:41:01 +0400

    Link replaced with NavLink

--- React/RF/src/components/Header.jsx
+++ React/RF/src/components/Header.jsx
@@ -1,13 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 export default function Header() {
   return (
     <div>
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-            <Link to="/about">About</Link>
-            <Link to='/vans'>Vans</Link>
-            <Link to="/host">Host</Link>
+            <NavLink to="/about"
+              className={({isActive}) => isActive ? "active-link" : null}
+            >About</NavLink>
+            <NavLink to='/vans'
+              className={({isActive}) => isActive ? "active-link" : null}
+            >Vans</NavLink>
+            <NavLink to="/host"
+              className={({isActive}) => isActive ? "active-link" : null}
+            >Host</NavLink>
         </nav>
       </header>
     </div>
